a9e19c377db7f0e09fd7c643fb3b9be3de8c7fa4
SaraDonzellini <saradonzie@gmail.com>
2022-11-22 16:00:16 +0100

    cleaning

--- js/script.js
+++ js/script.js
@@ -8,26 +8,22 @@ let ticketFull
 let ticketMinor
 let ticketSenior
 
-button.addEventListener('click', function() {
+button.addEventListener('click', function () {
   console.log(numberOfKm.value);
   console.log(passengerAge.value);
   console.log(ticketFull)
   console.log(ticketMinor)
   console.log(ticketSenior)
 
-  
   ticketFull = numberOfKm.value * pricePerKm;
   ticketMinor = ticketFull - (ticketFull * 0.175);
   ticketSenior = ticketFull - (ticketFull * 0.333);
-  
-
-
 
   if (passengerAge.value < 18) {
     document.getElementById('totale').innerHTML = ticketMinor.toFixed(2) + "&euro;"
   } else if (passengerAge.value >= 65) {
-      document.getElementById('totale').innerHTML = ticketSenior.toFixed(2) + "&euro;"
+    document.getElementById('totale').innerHTML = ticketSenior.toFixed(2) + "&euro;"
   } else
-      document.getElementById('totale').innerHTML = ticketFull.toFixed(2) + "&euro;"
+    document.getElementById('totale').innerHTML = ticketFull.toFixed(2) + "&euro;"
 });
 
