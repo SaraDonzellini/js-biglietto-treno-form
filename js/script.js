4090442bc59defdc708cc6dc3f00d82b30d2542b
SaraDonzellini <saradonzie@gmail.com>
2022-11-22 17:35:47 +0100

    cleaning code

--- js/script.js
+++ js/script.js
@@ -4,28 +4,24 @@ let passengerAge = document.getElementById('age');
 const pricePerKm = 0.2762;
 const button = document.getElementById('button');
 
-let ticketFull;
-let ticketMinor;
-let ticketSenior;
+
 
 button.addEventListener('click', function () {
+
+  let finalPrice;
   
-  ticketFull = numberOfKm.value * pricePerKm;
-  ticketMinor = ticketFull - (ticketFull * 0.175);
-  ticketSenior = ticketFull - (ticketFull * 0.333);
+  finalPrice = numberOfKm.value * pricePerKm;
 
   if (passengerAge.value < 18) {
-    document.getElementById('totale').innerHTML = ticketMinor.toFixed(2) + "&euro;";
+    finalPrice = finalPrice - (finalPrice * 0.175);
   } else if (passengerAge.value >= 65) {
-    document.getElementById('totale').innerHTML = ticketSenior.toFixed(2) + "&euro;";
-  } else
-    document.getElementById('totale').innerHTML = ticketFull.toFixed(2) + "&euro;";
+    finalPrice = finalPrice - (finalPrice * 0.333);
+  }
+  
+  document.getElementById('totale').innerHTML = finalPrice.toFixed(2) + "&euro;";
 
   console.log(numberOfKm.value);
   console.log(passengerAge.value);
-  console.log(ticketFull);
-  console.log(ticketMinor);
-  console.log(ticketSenior);
 
 });
 
